e690042a2cfeaa2f33bacf8222e9204880c004cc
Oleksandr Filenkov <w69935@student.wsiz.edu.pl>
2024-10-17 11:23:56 +0200

    first commit

---
+++ ConsoleApp1/Constructions/Construction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Constructions
+{
+    internal class Construction
+    { 
+        public Construction()
+        {
+            
+        }
+        public Construction(float height, float width, int entrances, int humancapacity, string buildmaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humancapacity;
+            BuildMaterial = buildmaterial;
+        }
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+    }
+}
+//1.Height typu zmiennoprzecinkowego
+//2. Width typu zmiennoprzecinkowego
+//3. Entrances typu liczba całkowita
+//4. HumanCapacity typu liczba całkowita
+//5. BuildMaterial typu tekstowego
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOP.Constructions;
+var construction1 = new Construction();
+var construction2 = new Construction(23.0f, 45.0f, 4, 4, "Wood");
+Console.WriteLine("Hello");
